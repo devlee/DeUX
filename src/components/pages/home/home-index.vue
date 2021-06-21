9d7f346d18dbace3f5600ec1d9e900847664e348
devlee <devlee@outlook.com>
2021-06-21 14:13:44 +0800

    init

---
+++ src/components/pages/home/home-index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='home-index'>home 123</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'home-index',
+  steup(props, context) {
+
+  }
+})
+</script>
+<style lang="less" scoped>
+.home-index {
+  color: red;
+}
+</style>
